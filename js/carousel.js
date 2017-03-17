434e92c483411d839d54f199215ee509583781ef
Justin Mi <justin.mi@berkeley.edu>
2017-03-16 20:07:53 -0700

    changed css

--- js/carousel.js
+++ js/carousel.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-    $('.single-item').slick();
+    $('.single-item').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        adaptiveHeight: true,
+        variableWidth: true
+    });
 });
