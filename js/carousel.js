31b8a2ff99dcfdca66ae534cd2e86b4f25800134
Justin Mi <justin.mi@berkeley.edu>
2017-03-16 20:12:23 -0700

    changed carousel

--- js/carousel.js
+++ js/carousel.js
@@ -5,8 +5,6 @@ $(document).ready(function(){
         speed: 300,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
         adaptiveHeight: true,
         variableWidth: true
     });
